59be654aa1118dc4e49714cddd4ad43d2fa34be9
dewit777 <23726598@student.g.nwu.ac.za>
2017-08-23 22:53:04 +0200

    Website Intial creating
    Basic Layout

---
+++ ITRW324/ITRW324/FileUploadfrm.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FileUploadfrm.aspx.cs" Inherits="ITRW324.FileUploadfrm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:FileUpload ID="FileUploadVerify" runat="server" enctype="multipart/form-data"/>
+        <br />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btnsubmit" runat="server" OnClick="btnsubmit_Click" Text="Submit" />
+        <br />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ITRW324/ITRW324/FileUploadfrm.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+
+
+namespace ITRW324
+{
+    public partial class FileUploadfrm : System.Web.UI.Page
+    {
+        string hash;
+        string file;
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            var Sha = new SHA256Managed();
+            if (FileUploadVerify.HasFile)
+            {
+
+                try
+                {
+                    file = FileUploadVerify.FileName;
+                    //  FileUploadVerify.SaveAs(("C:/Users/deWit777/Desktop/upload/") + file);
+                    sb.AppendFormat("<br/> Save As: {0}", FileUploadVerify.PostedFile.FileName);
+                    sb.AppendFormat("<br/> File type: {0}", FileUploadVerify.PostedFile.ContentType);
+                    sb.AppendFormat("<br/> File length: {0}", FileUploadVerify.PostedFile.ContentLength);
+                    sb.AppendFormat("<br/> File name: {0}", FileUploadVerify.PostedFile.FileName);
+
+                    HttpPostedFile myFile = FileUploadVerify.PostedFile;
+                    int nfilelen = myFile.ContentLength;
+                    byte[] myData = new byte[nfilelen];
+                    myFile.InputStream.Read(myData, 0, nfilelen);
+
+                    hash = BitConverter.ToString(Sha.ComputeHash(myData));
+                    sb.AppendFormat("<br/> File hashcode: {0}", hash);
+
+                    Label1.Text = sb.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    Label1.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
+                }
+             
+            }
+            else
+            {
+                Label1.Text = "Please select file to upload";
+                }
+           
+
+
+               
+              
+                
+               
+            
+        }
+    }
+}
