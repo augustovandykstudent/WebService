eb236fe513df066ef7ca5b584782dd0e15967abb
Augusto <26603349@student.g.nwu.ac.za>
2017-10-21 01:57:09 +0200

    Added a new structure for the blockchain

---
+++ New Blockchain Structure with example project on how to use/BlockChainTcpServer/BlockChainTcpServer/Block.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockChainTcpServer
+{
+    class Block
+    {
+        public string _sTimeStamp, _sHash, _sUserID;
+        public Block _oNext;
+
+        public Block()
+            : this (null, null, null, null)
+        {          
+        }
+
+        public Block(string sTimeStamp, string sHash, string sUserID)
+            : this(sTimeStamp, sHash, sUserID, null)
+        {
+        }
+
+        public Block(string sTimeStamp, string sHash, string sUserID, Block oPtr)
+        {
+            _sTimeStamp = sTimeStamp;
+            _sHash = sHash;
+            _sUserID = sUserID;
+            _oNext = oPtr;
+        }
+    }
+}
---
+++ New Blockchain Structure with example project on how to use/BlockChainTcpServer/BlockChainTcpServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BlockChainTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockChain chain = new BlockChain();
+            chain.AddToHead(Convert.ToString(System.DateTime.Now), "BlahBlah", Convert.ToString(1));
+            chain.Append(Convert.ToString(System.DateTime.Now), "DroopDroop", Convert.ToString(1));
+            chain.ShowBlockChain();
+            Console.ReadLine();//pause
+        }
+
+        public bool Validate(string sHash)
+        {
+            return true;
+        }
+    }
+}
