acb0bc946e80bbc6ed38e50dad2136c640a40ffe
NeandreFourie <25927949@student.g.nwu.ac.za>
2017-10-23 01:17:43 +0200

    Added functionality
    Added validate function
    Fixed Submit function
    Implemented webservice into validate and Submit

--- ITRW324/ITRW324/FileUploadfrm.aspx.cs
+++ ITRW324/ITRW324/FileUploadfrm.aspx.cs
@@ -12,6 +12,7 @@ using System.Configuration;
 using MySql.Data.MySqlClient;
 using System.Net;
 using ITRW324.ServiceReference1;
+using ITRW324.ServiceReference2;
 
 
 namespace ITRW324
@@ -23,6 +24,7 @@ namespace ITRW324
         public int length, userid;
         byte[] myData;
         ServiceReference1.ServiceSoapClient webservice = new ServiceReference1.ServiceSoapClient();
+        ServiceReference2.ServiceSoapClient webservice2 = new ServiceReference2.ServiceSoapClient();
         string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
 
         protected void OnMenuItemDataBound(object sender, MenuEventArgs e)
@@ -46,7 +48,6 @@ namespace ITRW324
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["user"] == null)
-                // Response.Redirect("Login.aspx");
                 Label1.Text = "please login";
             else
             {
@@ -61,43 +62,6 @@ namespace ITRW324
 
         protected void btnVerify_Click(object sender, EventArgs e)
         {
-            checkifexist();
-            Label1.Text = "";
-            StringBuilder sb = new StringBuilder();
-            var Sha = new SHA256Managed();
-
-            string date = DateTime.Now.ToString("dd-mm-yy");
-            Stream fs = FileUploadVerify.PostedFile.InputStream;
-            BinaryReader br = new BinaryReader(fs);
-            myData = br.ReadBytes((Int32)fs.Length);
-            hash = BitConverter.ToString(Sha.ComputeHash(myData));
-            sb.AppendFormat("<br/> File hashcode: {0}", hash);
-            Label1.Text = sb.ToString();
-            Block tmp = new Block();
-            tmp.BlockAdd(hash, file);
-
-            if (checkifexist() == false)
-            {
-                if (type == "application/pdf")
-                {
-                    webservice.Insert(file, type, date, hash, myData, userid);
-                }
-                else
-                {
-                    Label1.Text = "Only PDF allowed";
-                }
-
-            }
-            else
-            {
-                Label1.Text = "Exists";
-
-            }
-
-        }
-
-        protected void btnsubmit_Click(object sender, EventArgs e)
-        {
             Label1.Text = "";
             StringBuilder sb = new StringBuilder();
             var Sha = new SHA256Managed();
@@ -106,8 +70,6 @@ namespace ITRW324
 
                 try
                 {
-
-                    //FileUploadVerify.SaveAs(("C:/Users/Neand/Desktop/upload/") + file);
                     file = FileUploadVerify.PostedFile.FileName;
                     sb.AppendFormat("<br/> Save As: {0}", file);
                     type = FileUploadVerify.PostedFile.ContentType;
@@ -125,15 +87,13 @@ namespace ITRW324
                     myData = br.ReadBytes((Int32)fs.Length);
                     hash = BitConverter.ToString(Sha.ComputeHash(myData));
                     sb.AppendFormat("<br/> File hashcode: {0}", hash);
-                    //  sb.AppendFormat("<br/> File data: {0}", data);
                     Label1.Text = sb.ToString();
-                    Block tmp = new Block();
-                    tmp.BlockAdd(hash,file);
-                    if (checkifexist()==false)
+
+                    if (webservice2.Validate(hash) == false)
                     {
                         if (type == "application/pdf")
                         {
-                            webservice.Insert(file, type, date, hash, myData, userid);
+                            Label1.Text = "PDF is not original, or is not yet added to our database";
                         }
                         else
                         {
@@ -150,6 +110,67 @@ namespace ITRW324
                 }
                 catch (Exception ex)
                 {
+                    Label1.Text = "Upload status: The file could not be Validated. The following error occured: " + ex.Message;
+                }
+
+            }
+            else
+            {
+                Label1.Text = "Please select file to Validate";
+            }
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            StringBuilder sb = new StringBuilder();
+            var Sha = new SHA256Managed();
+            if (FileUploadVerify.HasFile)
+            {
+
+                try
+                {
+                    file = FileUploadVerify.PostedFile.FileName;
+                    sb.AppendFormat("<br/> Save As: {0}", file);
+                    type = FileUploadVerify.PostedFile.ContentType;
+                    sb.AppendFormat("<br/> File type: {0}", type);
+                    length = FileUploadVerify.PostedFile.ContentLength;
+                    sb.AppendFormat("<br/> File length: {0}", length);
+                    sb.AppendFormat("<br/> File name: {0}", file);
+
+                    HttpPostedFile myFile = FileUploadVerify.PostedFile;
+
+                    
+                    string date = DateTime.Now.ToString("dd-mm-yy");
+                    Stream fs = FileUploadVerify.PostedFile.InputStream;
+                    BinaryReader br = new BinaryReader(fs);
+                    myData = br.ReadBytes((Int32)fs.Length);
+                    hash = BitConverter.ToString(Sha.ComputeHash(myData));
+                    sb.AppendFormat("<br/> File hashcode: {0}", hash);
+                    Label1.Text = sb.ToString();
+                    webservice2.AddToBlockChain(hash,file);
+
+                    if (webservice2.Validate(hash)==false)
+                    {
+                        if (type == "application/pdf")
+                        {
+                            webservice2.Insert(file, type, date, hash, myData, userid);
+                        }
+                        else
+                        {
+                            Label1.Text = "Only PDF allowed";
+                        }
+
+                    }
+                    else
+                    {
+                        Label1.Text = "Uploaded";
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
                     Label1.Text = "Upload status: The file could not be uploaded. The following error occured: " + ex.Message;
                 }
 
@@ -159,78 +180,7 @@ namespace ITRW324
                 Label1.Text = "Please select file to upload";
             }
 
-
-
-
-
         }
-
-        //Check if pdf hash exists in database
-        public bool checkifexist()
-        {
-            MySqlConnection conn = new MySqlConnection(constr);
-            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Documents WHERE Hash =" + hash, conn);
-            conn.Open();
-            MySqlDataReader dr = cmd.ExecuteReader();
-            while (dr.Read())
-            {
-                if (dr.HasRows == true)
-                {
-                   
-                    return true;
-                }
-                
-            }
-            return false;
-
-           
-        }
-
-        //Upload pdf to database
-        /*public void upload(string sName, string sType, string sCreationDate, string sHash, byte[] bData, int iUserID)
-        {
-            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-            try
-            {
-                MySqlConnection conn = new MySqlConnection(constr);
-
-                string time = DateTime.Now.ToString("yyyy-MM-dd"); ;
-
-                
-
-                string insert = "INSERT INTO Documents (File_name,Type,Creation_date,Hash, Data) VALUES ('" + sName +"','" + sType +"','" + sCreationDate +"','" + sHash +"'," + bData +", " + iUserID + ")";
-                using (MySqlCommand cmd = new MySqlCommand(insert, conn))
-                {
-                    cmd.Connection = conn;
-
-                    
-                    using (MySqlDataAdapter adpt = new MySqlDataAdapter())
-                    {
-                        adpt.SelectCommand = cmd;
-                        //   cmd.Parameters.AddWithValue("@ID", test);
-                        cmd.Parameters.AddWithValue("@Name", file);
-                        cmd.Parameters.AddWithValue("@Type", type);
-                        cmd.Parameters.AddWithValue("@Creationdate", time);
-
-                        cmd.Parameters.AddWithValue("@Hash", hash);
-                        // cmd.Parameters.AddWithValue("@Previoushash", test2);
-                        cmd.Parameters.AddWithValue("@Data", myData);
-
-
-                        conn.Open();
-                        int result = cmd.ExecuteNonQuery();
-                        conn.Close();
-
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-                Label1.Text = "Not Entered " + ex;
-            }
-        }  */  
-      
     }
 
     
