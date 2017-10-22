b752ae56845023dd557993b3fb91f0b419938719
Mardus <24569313@student.g.nwu.ac.za>
2017-10-23 00:52:27 +0200

    s

--- Die-Validator/Die-Validator/Form1.cs
+++ Die-Validator/Die-Validator/Form1.cs
@@ -9,6 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+using BlockChainTcpServer;
 
 namespace Die_Validator
 {
