640a12d2da83e2344604f370cabed288a1ff86ae
Mardus <24569313@student.g.nwu.ac.za>
2017-10-23 02:08:13 +0200

    New name

---
+++ New Blockchain Struct/BlockChainTcpServer/BlockChainTcpServer/BlockChain.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BlockChainTcpServer
+{
+    [Serializable()]
+
+    public class ObjectToSerialize:ISerializable
+    {
+        private BlockChain blockChain;
+
+        public BlockChain BlockChain
+        {
+            get { return this.blockChain; }
+            set { this.blockChain = value; }
+        }
+
+        public ObjectToSerialize()
+        {
+
+        }
+
+        public ObjectToSerialize(SerializationInfo info, StreamingContext ctxt)
+        {
+            this.blockChain = (BlockChain)info.GetValue("BlockChain", typeof(BlockChain));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("BlockChain", this.blockChain);
+        }
+    }
+
+    [Serializable()]
+    public class Serializer
+    {
+        public Serializer()
+        {
+        }
+
+        public void SerializeObject(string fileName, ObjectToSerialize objectToSerialize)
+        {
+            Stream stream = File.Open(fileName, FileMode.Create);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            bFormatter.Serialize(stream, objectToSerialize);
+            stream.Close();
+        }
+
+        public ObjectToSerialize DeSerializeObject(string fileName)
+        {
+            ObjectToSerialize objectToSerialize;
+            Stream stream = File.Open(fileName, FileMode.Open);
+            BinaryFormatter bFormatter = new BinaryFormatter();
+            objectToSerialize = (ObjectToSerialize)bFormatter.Deserialize(stream);
+            stream.Close();
+            return objectToSerialize;
+        }
+    }
+
+    [Serializable()]
+    public class BlockChain:ISerializable 
+    {
+        private Block head, tail;
+        private BlockChain chain;
+
+        public BlockChain()
+        {
+            head = null;
+            tail = null;
+        }
+
+        public bool AddToHead(string sTimeStamp, string sHash, string sUserID)// method that adds first block to blockchain
+        {
+            head = new Block(sTimeStamp, sHash, sUserID);
+            if (tail == null)
+            {
+                tail = head;
+            }
+            return true;
+        }
+
+        public bool AddToTail(string sTimeStamp, string sHash, string sUserID)
+        {
+            if (!IsEmpty())
+            {
+                tail._oNext = new Block(sTimeStamp, sHash, sUserID);
+                tail = tail._oNext;
+            }
+            else
+                head = tail = new Block(sTimeStamp, sHash, sUserID);
+            return true;
+        }
+
+        public bool Append(string sTimeStamp, string sHash, string sUserID)
+        {
+            Block block = new Block(sTimeStamp, sHash, sUserID);
+            if (block == null)
+                return false;//ran out of memory
+            else
+            {
+                if(head == null)
+                {
+                    head = block;
+                    tail = block;
+                }
+                else
+                {
+                    tail._oNext = block;
+                    tail = block;
+                }
+            }
+            return true;
+        }
+
+        public bool IsEmpty()
+        {
+            if (head == null)
+                return true;
+            return false;
+        }
+
+        public void ShowBlockChain()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("null");
+                return;
+            }
+
+            Block ptr = head;
+            while (ptr != null)
+            {
+                Console.WriteLine("Time Stamp: " + ptr._sTimeStamp + " Hash: " +  ptr._sHash + " User ID: " + ptr._sUserID);
+                ptr = ptr._oNext;
+            }
+        }
+
+        private BlockChain(SerializationInfo info, StreamingContext ctxt)
+        {
+            this.head = (Block)info.GetValue("Head", typeof(Block));
+            this.tail = (Block)info.GetValue("Tail", typeof(Block));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("Head", this.head);
+            info.AddValue("Tail", this.tail);
+        }
+    }    
+}
---
+++ New Blockchain Struct/BlockChainTcpServer/BlockChainTcpServer/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BlockChainTcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockChain chain = new BlockChain();
+            chain.AddToHead(Convert.ToString(System.DateTime.Now), "BlahBlah", Convert.ToString(1));
+            chain.Append(Convert.ToString(System.DateTime.Now), "Good Good", Convert.ToString(2));
+            chain.Append(Convert.ToString(System.DateTime.Now), "DroopDroop", Convert.ToString(4));
+            chain.ShowBlockChain();
+            Console.WriteLine("Serialization");
+
+            ObjectToSerialize objectToSerialize = new ObjectToSerialize();
+            objectToSerialize.BlockChain = chain;
+
+            Serializer serializer = new Serializer();
+            serializer.SerializeObject(@"D:\Data\outputFile.txt", objectToSerialize);
+
+            chain = null;
+
+            GetReference().AddToBlockChain("123", "1");
+
+            //deserialization
+            byte[] bChain = GetReference().GetBlockChain();
+            chain = objectToSerialize.BlockChain;
+
+            chain.ShowBlockChain();
+
+            Console.ReadLine();//pause
+        }
+
+        public bool Validate(string sHash)
+        {
+            return true;
+        }
+
+        private BlockChain Deserialize(byte[] param)
+        {
+            if (param == null)
+                return null;
+            BinaryFormatter bf = new BinaryFormatter();
+            MemoryStream ms = new MemoryStream(param);
+            BlockChain chain = (BlockChain)bf.Deserialize(ms);
+
+            return chain;
+            
+        }
+
+        private static ServiceReference1.ServiceSoapClient GetReference()
+       {
+            return new ServiceReference1.ServiceSoapClient();
+        }
+    }
+}
